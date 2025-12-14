958f3effa9a3a6fc56cbf9d9497ddc8c5981d724
mariancosmin17 <161645867+mariancosmin17@users.noreply.github.com>
2025-12-14 21:24:32 +0200

    initializare ract+adaugare slider pentru a selecta caloriile

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Form
 from PIL import Image
 import io
 from app.ml.calories import CalorieCalculator
 from app.ml.model import FoodClassifier
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 classifier = FoodClassifier()
 calorie_calculator=CalorieCalculator("calorie_table.csv")
@@ -14,11 +22,10 @@ def root():
     return {"message": "Food AI API is running"}
 
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
+async def predict(file: UploadFile = File(...),grams:int=Form(...,ge=1,le=2500)):
     contents = await file.read()
     image = Image.open(io.BytesIO(contents)).convert("RGB")
     label = classifier.predict(image)
-    grams=250 #pentru demo
     calories=calorie_calculator.calculate(label,grams)
     return {
         "prediction": label,
