880c2bfec082a10c6af80fe413f767d691c9e833
mariancosmin17 <161645867+mariancosmin17@users.noreply.github.com>
2025-12-22 17:15:32 +0200

    Adaugare rute pentru login/register

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, File, Form,Depends, HTTPException, status
 from PIL import Image
-from app.database import Base, engine
+from sqlalchemy.orm import Session
+from app.core.security import hash_password,verify_password, create_access_token
+from app.db.models import User
+from app.db.database import Base, engine,SessionLocal
 import io
 from app.ml.calories import CalorieCalculator
 from app.ml.model import FoodClassifier
 from fastapi.middleware.cors import CORSMiddleware
+
+from backend.app.db.database import SessionLocal
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -18,6 +23,39 @@ app.add_middleware(
 
 classifier = FoodClassifier()
 calorie_calculator=CalorieCalculator("calorie_table.csv")
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/register")
+def register(username:str,password:str,db:Session=Depends(get_db)):
+    existing_user=db.query(User).filter(User.username==username).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already registered."
+        )
+    hashed_password=hash_password(password)
+    user=User(username=username,hashed_password=hashed_password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return {"message": "User created successfully"}
+
+@app.post("/login")
+def login(username:str,password:str,db:Session=Depends(get_db)):
+    user=db.query(User).filter(User.username==username).first()
+    if not user or not verify_password(password,user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password"
+        )
+    acces_token=create_access_token(data={"sub":user.username})
+    return {"acces_token":acces_token,"token_type":"bearer"}
 
 @app.get("/")
 def root():
