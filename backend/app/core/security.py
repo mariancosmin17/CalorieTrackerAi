a64a17112d5c6b911ed7c4f592e5d1ceff2f3145
mariancosmin17 <161645867+mariancosmin17@users.noreply.github.com>
2025-12-23 13:48:42 +0200

    Adaugare verificare jwt,verificare autentifcare pe ruta predict+mici buguri rezolvate

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -4,20 +4,20 @@ from datetime import datetime, timedelta
 
 pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
 SECRET_KEY="supersecretkey"
-ALGHORITM="HS256"
-ACCES_TOKEN_EXPIRE_MINUTES=30
+ALGORITHM="HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES=30
 
-def has_password(password:str)->str:
+def hash_password(password:str)->str:
     return pwd_context.hash(password)
 def verify_password(plain_password:str,hashed_password:str)->bool:
     return pwd_context.verify(plain_password,hashed_password)
-def create_acces_token(data:dict)->str:
+def create_access_token(data:dict)->str:
     to_encode=data.copy()
-    expire=datetime.utcnow()+timedelta(minutes=ACCES_TOKEN_EXPIRE_MINUTES)
+    expire=datetime.utcnow()+timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp":expire})
-    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGHORITM)
-def decode_acces_token(token:str)->dict:
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+def decode_access_token(token:str)->dict:
     try:
-        return jwt.decode(token,SECRET_KEY,algorithms=[ALGHORITM])
+        return jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
     except JWTError:
         return None
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,49 @@
-from fastapi import FastAPI, UploadFile, File, Form,Depends, HTTPException, status
+from fastapi import FastAPI, UploadFile, File, Form, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from PIL import Image
 from sqlalchemy.orm import Session
-from app.core.security import hash_password,verify_password, create_access_token
-from app.db.models import User
-from app.db.database import Base, engine,SessionLocal
+from app.core.security import decode_access_token, hash_password, verify_password, create_access_token
+from app.db.models import User, History
+from app.db.database import Base, engine, SessionLocal
 import io
-from app.ml.calories import CalorieCalculator
+from app.ml. calories import CalorieCalculator
 from app.ml.model import FoodClassifier
-from fastapi.middleware.cors import CORSMiddleware
-
-from backend.app.db.database import SessionLocal
+from fastapi. middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = decode_access_token(token)
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+        )
+    username = payload.get("sub")
+    if not username:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+        )
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found",
+        )
+    return user
+
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
@@ -22,17 +53,13 @@ app.add_middleware(
 )
 
 classifier = FoodClassifier()
-calorie_calculator=CalorieCalculator("calorie_table.csv")
+calorie_calculator = CalorieCalculator("calorie_table.csv")
 
-def get_db():
-    db=SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
+@app.get("/")
+def root():
+    return {"message": "Food AI API is running"}
 @app.post("/register")
-def register(username:str,password:str,db:Session=Depends(get_db)):
+def register(username:str=Form(...),password:str=Form(...),db:Session=Depends(get_db)):
     existing_user=db.query(User).filter(User.username==username).first()
     if existing_user:
         raise HTTPException(
@@ -47,28 +74,34 @@ def register(username:str,password:str,db:Session=Depends(get_db)):
     return {"message": "User created successfully"}
 
 @app.post("/login")
-def login(username:str,password:str,db:Session=Depends(get_db)):
+def login(username:str=Form(...),password:str=Form(...),db:Session=Depends(get_db)):
     user=db.query(User).filter(User.username==username).first()
     if not user or not verify_password(password,user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password"
         )
-    acces_token=create_access_token(data={"sub":user.username})
-    return {"acces_token":acces_token,"token_type":"bearer"}
-
-@app.get("/")
-def root():
-    return {"message": "Food AI API is running"}
+    access_token=create_access_token(data={"sub":user.username})
+    return {"access_token":access_token,"token_type":"bearer"}
 
 @app.post("/predict")
-async def predict(file: UploadFile = File(...),grams:int=Form(...,ge=1,le=2500)):
+async def predict(file: UploadFile = File(...),grams:int=Form(...,ge=1,le=2500),
+                  db:Session=Depends(get_db),current_user:User=Depends(get_current_user)):
+
     contents = await file.read()
     image = Image.open(io.BytesIO(contents)).convert("RGB")
     label = classifier.predict(image)
     calories=calorie_calculator.calculate(label,grams)
+
+    history=History(label=label,grams=grams,calories=calories,user_id=current_user.id)
+    db.add(history)
+    db.commit()
     return {
         "prediction": label,
         "grams":grams,
         "calories":calories
     }
+@app.get("/history")
+def get_history(db:Session=Depends(get_db),current_user=Depends(get_current_user)):
+    history=(db.query(History).filter(History.user_id==current_user.id).order_by(History.created_at.desc()).all())
+    return {"history":history}
