85016ff9a752be265cba874808e22e22d0633ece
mariancosmin17 <161645867+mariancosmin17@users.noreply.github.com>
2026-03-10 12:52:03 +0200

    Retusuri si reorganizare

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -18,7 +18,7 @@ export function Sidebar({isOpen,onClose}){
             label:'Profile',
             icon:UserIcon,
             onClick:()=>{
-                navigate('/settings');
+                navigate('/settings/personal-info');
                 onClose();
                 },
             },
@@ -27,7 +27,7 @@ export function Sidebar({isOpen,onClose}){
             label:'Settings',
             icon:Cog6ToothIcon,
             onClick:()=>{
-                console.log('urmeaza');
+                navigate('/settings');
                 onClose();
                 },
             },
