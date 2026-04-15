21e7c1c3ff9e0b207b65ac9c7bb507b225495344
mariancosmin17 <161645867+mariancosmin17@users.noreply.github.com>
2026-04-15 17:45:40 +0300

    Modificare la config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,17 +6,13 @@ class Settings(BaseSettings):
     SECRET_KEY:str
     ALGORITHM:str="HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES:int=30
-    SMTP_HOST:str="smtp.gmail.com"
-    SMTP_PORT:int=587
-    SMTP_USERNAME:str
-    SMTP_PASSWORD:str
-    SMTP_FROM_EMAIL:str
     SMTP_FROM_NAME:str="Food AI App"
     RESET_CODE_EXPIRE_MINUTES:int=15
     GEMINI_API_KEY:str=""
     GEMINI_MODEL:str="gemini-3-flash-preview"
     YOLO_MODEL_PATH:str="runs/detect/allinone_yolov8n_v1/weights/best.pt"
-
+    RESEND_API_KEY:str=""
+    SUPPORT_EMAIL:str=""
     class Config:
         env_file=".env"
 
