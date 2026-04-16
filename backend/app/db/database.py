d5dc484b27a8b3120211da834dfc832ceb1fc4c2
mariancosmin17 <161645867+mariancosmin17@users.noreply.github.com>
2026-04-16 12:46:28 +0300

    Baza de date in cloud (Neon)

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
 
-DATABASE_URL="sqlite:///./calorie_tracker.db"
-engine=create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-Base=declarative_base()
+DATABASE_URL = settings.DATABASE_URL
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
